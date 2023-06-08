3655d1cad10aa91aa9a920e688ccc998cea2afdf
Vijayakumar A <36878324+kumvijaya@users.noreply.github.com>
2023-06-08 20:49:22 +0530

    Update copy_folder_tree.py

--- copy_folder_tree.py
+++ copy_folder_tree.py
@@ -1,56 +1,53 @@
-"""  Python – Copy Directory Structure Without Files """
+"""  This is to copy the folder structure without files """
 
 import shutil
 import os
 import argparse
 
-def user_input():
-    """To get inputs from user :- Source Directory and Destination Directory
+def get_user_inputs():
+    """Gets the inputs from user (Source Directory and Destination Directory)
 
     Returns:
-        String: Source and Destination converted from list to string 
+        Items: Source and Destination folder inputs
     """    
     argparser = argparse.ArgumentParser(
-            prog='create_folders_without_files',
-            description='Copy Directory Structure Without Files')
-    argparser.add_argument('sourcedirectory', type=str, nargs=1,
-                            help='Directory Structure With Files')
-    argparser.add_argument('destdirectory', type=str, nargs=1,
-                            help='Directory Structure Without Files')
+            prog='copy_folder_tree',
+            description='Copy folder Tree')
+    argparser.add_argument('src', type=str, nargs=1,
+                            help='Source folder')
+    argparser.add_argument('dest', type=str, nargs=1,
+                            help='Destination folder')
 
     args = argparser.parse_args()
-    srcdirectory = args.sourcedirectory[0]
-    destdirectory = args.destdirectory[0]
-    return (srcdirectory,destdirectory)
+    return (args.src[0], args.dest[0])
 
-def ignore_files(dir, files):
-    """To ignore the files if present in any folders
+def get_files(dir, content):
+    """To get the files if present in given folder
 
     dir:
-        dir (String): Directory
-        files (String): Files under the directory
+        dir (String): Folder
+        files (String): content under the folder
 
     Returns:
-        Directory(Full Path) : String Format
+        list : Files in given directory
     """
-    return [f for f in files if os.path.isfile(os.path.join(dir, f))]
+    return [f for f in content if os.path.isfile(os.path.join(dir, f))]
 
-def copy_without_files(srcdir, destdir):
-    """Source Directory Structure copied to Destination Without Files
+def copy_tree(src, dest):
+    """Copies source folder to destination folder without files
 
     Args:
-        srcdirectory (String): Input from user - Source Directory with files
-        destdirectory (String): Input from user - Destination Directory - Directory copied without Files
+        src (String): Source folder
+        dest (String): Destination folder
     
     Exception:
-        Exception raised if Destination file already exists.
+        Prints the message if destination folder already exists.
 
     """
     try:
-        shutil.copytree(srcdir, destdir, ignore=ignore_files)
+        shutil.copytree(src, dest, ignore=get_files)
     except FileExistsError:
         print('The directory already exists')
 
-#Get Source and Destination to generate the destination directory without files
-s_directory, d_directory = user_input()
-copy_without_files(s_directory, d_directory)
+src, dest = get_user_inputs()
+copy_tree(src, dest)
